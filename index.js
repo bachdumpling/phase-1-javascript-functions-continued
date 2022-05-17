d494ca6fdac747b65ee11fffac499cf209e985f9
Bach Le <lehoangbach7802@gmail.com>
2022-05-17 12:39:55 -0400

    commit tree 0491474b95e21fdbe91f937eff835ad41f6dc8db

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+
+function wrapAdjective(highlight = '*') {
+    return function encouragingPromptFunction(adjective = 'special') {
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
+wrapAdjective('%')('a dedicated programmer');
